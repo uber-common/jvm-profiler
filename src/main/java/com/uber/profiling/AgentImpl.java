3be6dfce64c0e36d3fea1905ba43ff2f6ed3d884
Bo Yang <boy@uber.com>
2018-06-27 14:46:12 -0700

    Update AgentImpl version

--- src/main/java/com/uber/profiling/AgentImpl.java
+++ src/main/java/com/uber/profiling/AgentImpl.java
@@ -46,7 +46,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class AgentImpl {
-    public static final String VERSION = "0.0.6";
+    public static final String VERSION = "0.0.7";
     
     private static final AgentLogger logger = AgentLogger.getLogger(AgentImpl.class.getName());
 
