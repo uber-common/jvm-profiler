853e8f1430c44950363869fbbb310c0a2eeba2b7
Bo Yang <boy@uber.com>
2018-09-05 15:35:51 -0700

    Increase string max size to 800000

--- src/main/java/com/uber/profiling/AgentImpl.java
+++ src/main/java/com/uber/profiling/AgentImpl.java
@@ -46,7 +46,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class AgentImpl {
-    public static final String VERSION = "0.0.8";
+    public static final String VERSION = "0.0.9";
     
     private static final AgentLogger logger = AgentLogger.getLogger(AgentImpl.class.getName());
 
